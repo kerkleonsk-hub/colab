17b450a21382189788ad5116404f792a8fdd9bcf
Leonenko <kerkleonsk@gmail.com>
2025-12-16 17:39:08 +0300

    Добавил переменную

---
+++ copy.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+Just deleted some symbols
+int main() {
+    double INTERVAL_START = 0.0;
+    double INTERVAL_END = 6.0;
+    added const
+    double s = 12.0
